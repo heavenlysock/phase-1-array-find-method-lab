cc5b7c429e14858b80f32bfc6b0868d8ef58249c
bobertsworld <bobertsworld@gmail.com>
2022-11-07 07:30:38 -0800

    commit tree 8933319acfae8831cef99c326621c2edb918bbac

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
+
+
+superbowlWin = (record) => {
+
+    const result = record.find(record => record.result === "W" );
+    return!!result? result.year : undefined;
+    
+}
